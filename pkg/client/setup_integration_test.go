ef21cf18110f20ecd8c02a6fa1ad654b745b0fef
Tim Chmielecki <me@timcki.com>
2025-01-07 14:31:58 +0000

    fix(test): fix broken import path

--- pkg/client/setup_integration_test.go
+++ pkg/client/setup_integration_test.go
@@ -16,7 +16,7 @@ import (
 	"os"
 	"time"
 
-	"github.com/TokenTax/etherscan-api/v2/internal/chain"
+	"github.com/TokenTax/etherscan-api/v2/pkg/chain"
 )
 
 const apiKeyEnvName = "ETHERSCAN_API_KEY"
